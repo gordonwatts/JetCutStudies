58e697a5e1b45b5ae10bf4e41d418b9561861234
Gordon Watts <gordon@gordonandpaula.net>
2015-11-15 00:52:21 -0800

    Should not have been there in the first place!

--- start_JetCutStudies.bat
+++
@@ -1,4 +0,0 @@
-@REM Use to start devenv
-@set ROOTSYS=C:\Users\gordo\AppData\Local\Temp\root\root-5.34.34-vc12
-@set PATH=C:\Users\gordo\AppData\Local\Temp\root\root-5.34.34-vc12\bin;%PATH%
-@C:\Users\gordo\Documents\Code\calratio2015\JetCutStudies\JetCutStudies.sln
