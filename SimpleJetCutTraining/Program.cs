915f2f9a6780113f0a863cbe31c8c94583d2a14b
Gordon Watts <gordon@gordonandpaula.net>
2016-02-06 23:29:38 +0100

    First go at writing out training data files

--- SimpleJetCutTraining/Program.cs
+++ SimpleJetCutTraining/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using DiVertAnalysis;
+using libDataAccess;
+using LINQToTTreeLib.Files;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using LINQToTTreeLib;
+using static System.Console;
 
 namespace SimpleJetCutTraining
 {
@@ -14,6 +16,48 @@ namespace SimpleJetCutTraining
     {
         static void Main(string[] args)
         {
+            WriteLine("Finding the files");
+            var backgroundEvents = Files.GetJ2Z();
+            //var signalHV125pi15Events = Files.Get125pi15();
+            //var signalHV125pi40Events = Files.Get125pi40();
+            var signalHV600pi100Events = Files.Get600pi100();
+
+            //
+            // Generate the output files.
+            //
+
+            WriteTrainingROOTFile(backgroundEvents, false, new FileInfo("J2Z.training.root"));
+            WriteTrainingROOTFile(signalHV600pi100Events, true, new FileInfo("HV600pi100.training.root"));
+
+            WriteLine($"Number of signalHV600pi100Events events: {signalHV600pi100Events.Count()}.");
+        }
+
+        /// <summary>
+        /// Generate training ROOT file output
+        /// </summary>
+        /// <param name="events">List of events we should watch over</param>
+        /// <param name="outputFile">Where to create an output ROOT file.</param>
+        private static FileInfo WriteTrainingROOTFile(IQueryable<recoTree> events, bool isSignal, FileInfo outputFile)
+        {
+            var trainingDataSetJets = events
+                .SelectMany(e => e.Jets);
+
+            if (isSignal)
+            {
+                trainingDataSetJets = trainingDataSetJets
+                    .Where(j => j.LLP.IsGoodIndex());
+            }
+
+            var trainingDataSet = trainingDataSetJets
+                .Select(j => new TrainingData()
+                {
+                    logR = j.logRatio,
+                    nTracks = (int)j.nTrk
+                });
+
+            trainingDataSet.AsTTree(outputFile);
+
+            return outputFile;
         }
     }
 }
---
+++ SimpleJetCutTraining/TrainingData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleJetCutTraining
+{
+    /// <summary>
+    /// Training data for simple jets
+    /// </summary>
+    class TrainingData
+    {
+        /// <summary>
+        /// The locR for the jet
+        /// </summary>
+        public double logR;
+
+        /// <summary>
+        /// The number of tracks.
+        /// </summary>
+        public int nTracks;
+    }
+}
