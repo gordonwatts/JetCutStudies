f8c8f862ca12f019ef834b5e9c08c938c6a67abf
Gordon Watts <gordon@gordonandpaula.net>
2016-02-08 15:28:11 +0100

    Update to latest version of LINQToTTree

--- SimpleJetCutTraining/Program.cs
+++ SimpleJetCutTraining/Program.cs
@@ -1,11 +1,11 @@
 ﻿using DiVertAnalysis;
 using libDataAccess;
-using LINQToTTreeLib.Files;
-using System.IO;
-using System.Linq;
 using LINQToTTreeLib;
 using TMVAUtilities;
+using System.Linq;
 using static System.Console;
+using static System.Math;
+using static LINQToTreeHelpers.ROOTUtils;
 
 namespace SimpleJetCutTraining
 {
@@ -28,9 +28,9 @@ namespace SimpleJetCutTraining
             //
 
             var t = TrainingIQueriable(signalHV600pi100Events, true)
-                .AsSignal()
-                .Background(TrainingIQueriable(backgroundEvents, false))
-                .BookMethod(ROOTNET.Interface.NTMVA.NTypes.EMVA.kCuts, "SimpleCuts", "V")
+                .AsSignal("HV600pi100")
+                .Background(TrainingIQueriable(backgroundEvents, false), "J2Z")
+                .BookMethod(ROOTNET.Interface.NTMVA.NTypes.EMVA.kCuts, "SimpleCuts", "!H:V:FitMethod=MC:EffSel:SampleSize=20000:VarProp[0]=FSmart:VarProp[1]=FSmart:VarTransform=Decorrelate")
                 .Train("VerySimpleTraining");
         }
 
@@ -43,25 +43,47 @@ namespace SimpleJetCutTraining
         /// <returns></returns>
         private static IQueryable<TrainingData> TrainingIQueriable(IQueryable<recoTree> events, bool isSignal)
         {
+#if false
+            var allJets = from e in events
+                          from j in e.Jets
+                          where j.pT > 30.0 && Abs(j.eta) < 2.4
+                          select new TrainingData()
+                          {
+                              logR = j.logRatio,
+                              lowestPtTrack = (from t in e.Tracks
+                                               where DeltaR2(t.eta, t.phi, j.eta, j.phi) < 0.2 * 0.2
+                                               orderby t.pT ascending
+                                               select t).First()
+
+                          };
+#endif
             // Look at all jets
             var trainingDataSetJets = events
-                .SelectMany(e => e.Jets);
+                .SelectMany(e => e.Jets.Select(j => new { Jet = j, Event = e }))
+                .Where(j => j.Jet.pT > 30.0)
+                .Where(j => Abs(j.Jet.eta) < 2.4);
 
             // If this is to be treated as signal, then look for the LLP to have
             // decayed in the right way.
             if (isSignal)
             {
                 trainingDataSetJets = trainingDataSetJets
-                    .Where(j => j.LLP.IsGoodIndex());
+                    .Where(j => j.Jet.LLP.IsGoodIndex());
             }
 
             // Fill our training data that will eventually be turned into the training tree.
-            var trainingDataSet = trainingDataSetJets
-                .Select(j => new TrainingData()
-                {
-                    logR = j.logRatio,
-                    nTracks = (int)j.nTrk,
-                });
+            var trainingDataSet = from j in trainingDataSetJets
+                                  let trkList = from t in j.Event.Tracks
+                                                where DeltaR2(t.eta, t.phi, j.Jet.eta, j.Jet.phi) < 0.04
+                                                orderby t.pT ascending
+                                                select t
+                                  where trkList.Any()
+                                  select new TrainingData()
+                                  {
+                                      logR = j.Jet.logRatio,
+                                      //nTracks = (int)j.Jet.nTrk,
+                                      lowestPtTrack = trkList.First().pT,
+                                  };
             return trainingDataSet;
         }
     }
