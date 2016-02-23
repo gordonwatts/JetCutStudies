4a51dd81e36e07116790c7d3d565b16f1b398ac0
Gordon Watts <gordon@gordonandpaula.net>
2016-02-23 13:20:01 +0100

    Thinking about infrastructure for training

---
+++ JetMVATraining/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JetMVATraining
+{
+    class Program
+    {
+        /// <summary>
+        /// Run the training for the MVA. This is run in a library,
+        /// so basically behind-this-guys-back. But it provides an easy single item to run.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Our data sources
+
+            // We want to be flat w.r.t. pT, so reweight.
+
+            // Finally, write out a tree for training everything.
+
+            // Now, do the training.
+
+            // And, finally, the testing.
+        }
+    }
+}
