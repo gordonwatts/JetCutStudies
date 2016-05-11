d8619a7a63ebaf6d9b169b98bbc3d85c81cd295d
Gordon Watts <gordon@gordonandpaula.net>
2016-05-10 20:31:44 -0700

    Use JetPt for our weighting

--- JetMVATraining/Program.cs
+++ JetMVATraining/Program.cs
@@ -54,11 +54,11 @@ namespace JetMVATraining
             {
                 // Create the training data and flatten the pT spectra.
                 var flatBackgroundTrainingData = backgroundTrainingTree
-                    .FlattenBySpectra(t => t.JetET, outputHistograms, "background")
+                    .FlattenBySpectra(t => t.JetPt, outputHistograms, "background")
                     ;
                 var flatSignalTrainingData = signalInCalOnly
                     .AsTrainingTree()
-                    .FlattenBySpectra(t => t.JetET, outputHistograms, "signal")
+                    .FlattenBySpectra(t => t.JetPt, outputHistograms, "signal")
                     ;
 
                 // Finally, plots of all the training input variables.
@@ -73,7 +73,7 @@ namespace JetMVATraining
                 var training = flatSignalTrainingData
                     .AsSignal(isTrainingEvent: e => e.EventNumber % 2 == 1)
                     .Background(flatBackgroundTrainingData, isTrainingEvent: e => e.EventNumber % 2 == 1)
-                    .UseVariables(t => t.JetET, t => t.CalRatio, t => t.MaxTrackPt, t => t.NTracks, t => t.SumPtOfAllTracks);
+                    .UseVariables(t => t.JetPt, t => t.CalRatio, t => t.MaxTrackPt, t => t.NTracks, t => t.SumPtOfAllTracks);
 
                 // Build options (like what we might like to transform.
                 var m1 = training.AddMethod(ROOTNET.Interface.NTMVA.NTypes.EMVA.kBDT, "BDT")
