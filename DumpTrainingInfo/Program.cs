e2f0edf6cfa09852de0acc9eb80b49b6483d3bd2
Gordon Watts <gordon@gordonandpaula.net>
2016-05-17 23:07:14 -0700

    Add a dumper that will dump out a particular event

---
+++ DumpTrainingInfo/Program.cs
@@ -0,0 +1,74 @@
+﻿using CalRatioTMVAUtilities;
+using JenkinsAccess;
+using libDataAccess;
+using libDataAccess.Utils;
+using LINQToTTreeLib;
+using LINQToTTreeLib.Files;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMVAUtilities;
+
+namespace DumpTrainingInfo
+{
+    class Program
+    {
+        /// <summary>
+        /// Pass an event number and basic training information will be dumped.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Parse the arguments.
+            CommandLineUtils.Parse(args);
+
+            // Get all the samples we want to look at, and turn them into
+            // jets with the proper weights attached for later use.
+
+            var backgroundJets = CommandLineUtils.GetRequestedBackground();
+
+            //var allSources = new List<Tuple<string, IQueryable<Files.MetaData>>>() {
+            //    Tuple.Create("600pi150lt9m", Files.Get600pi150lt9m().GenerateStream(1.0)),
+            //    Tuple.Create("400pi100lt9m", Files.Get400pi100lt9m().GenerateStream(1.0)),
+            //    Tuple.Create("200pi25lt5m", Files.Get200pi25lt5m().GenerateStream(1.0)),
+            //};
+
+            // The URL of the MVA we are going after
+            var mvaUrl = new Uri("http://jenks-higgs.phys.washington.edu:8080/job/CalR-JetMVATraining/372/artifact/Jet.MVATraining-JetPt.CalRatio.NTracks.SumPtOfAllTracks.MaxTrackPt_BDT.weights.xml");
+
+            // Get the weight file.
+            var weights = ArtifactAccess.GetArtifactFile(mvaUrl).Result;
+
+            // Now, create the MVA value from the weight file
+            var mvaValue = MVAWeightFileUtils.MVAFromWeightFile<TrainingTree>(weights);
+
+            // Go through the background jets and dump them.
+            var js = backgroundJets
+                .AsGoodJetStream()
+                .FilterNonTrainingEvents();
+
+            // Filter by event number
+            var jsGood = from j in js
+                          let evt = j.EventNumber
+                          where evt == 204133
+                          select j;
+
+            var jsWithTrainingInfo = from j in jsGood
+                                     let trainingInfo = TrainingUtils.TrainingTreeConverter.Invoke(j)
+                                     select new
+                                     {
+                                         EventNumber = j.EventNumber,
+                                         TrainingInfo = trainingInfo,
+                                         MVAValue = mvaValue.Invoke(TrainingUtils.TrainingTreeConverter.Invoke(j)),
+                                     };
+
+            // Dump the result to a CSV file
+            var outputFile = jsWithTrainingInfo
+                .AsCSV(new System.IO.FileInfo("TrainingInfo-Temp.csv"))
+                .CombineCSVFiles(new FileInfo("TrainingInfo.csv"));
+        }
+    }
+}
---
+++ TMVAUtilities/FileInfoUtils.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMVAUtilities
+{
+    /// <summary>
+    /// Some utiltiies
+    /// </summary>
+    public static class FileInfoUtils
+    {
+        /// <summary>
+        /// Given a servies of CSV files, combine them.
+        /// </summary>
+        /// <param name="files"></param>
+        /// <returns></returns>
+        public static FileInfo CombineCSVFiles(this FileInfo[] files, FileInfo outputName = null, bool allHaveTitltes = true)
+        {
+            if (files.Where(f => f.Extension != ".csv").Any())
+            {
+                throw new ArgumentException("At least one file passed to us has a file type that isn't csv!");
+            }
+
+            if (files.Length == 0)
+            {
+                throw new ArgumentException("No files were passed to CombineCSVFiles");
+            }
+
+            // Generate the output file
+            var outputFile = outputName == null
+                ? new FileInfo($"{files[0].FullName}")
+                : outputName;
+
+            // Now open each file
+            var linesToSkip = allHaveTitltes ? 1 : 0;
+
+            using (var outputWriter = outputFile.CreateText())
+            {
+                // The first file is special if there are titles. No matter what we copy them all over.
+                foreach (var line in files[0].AsLines())
+                {
+                    outputWriter.WriteLine(line);
+                }
+
+                // And for the rest of them.
+                foreach (var file in files.Skip(1))
+                {
+                    foreach (var line in file.AsLines().Skip(linesToSkip))
+                    {
+                        outputWriter.WriteLine(line);
+                    }
+                }
+                outputWriter.Close();
+            }
+
+            return outputFile;
+        }
+
+        /// <summary>
+        /// Read the files as lines, one by one.
+        /// </summary>
+        /// <param name="f"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> AsLines (this FileInfo f)
+        {
+            using (var reader = f.OpenText())
+            {
+                string line = "";
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+                    if (line != null)
+                    {
+                        yield return line;
+                    }
+                }
+            }
+        }
+    }
+}
