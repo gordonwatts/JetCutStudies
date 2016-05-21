f57d3079f4bd4fbdfc9bb32083feb60ef7c5908f
Gordon Watts <gordon@gordonandpaula.net>
2016-05-21 03:52:20 -0700

    Do re-weighting for input variables with MVA output - rescal the MVA weight to be 0 to 1 - new versino of LINQTOTTree to deal with C++ codegen bug

--- TrainingTestResults/Program.cs
+++ TrainingTestResults/Program.cs
@@ -101,7 +101,7 @@ namespace TrainingTestResults
         private static void PlotMVAResult(IQueryable<JetStream> source, FutureTDirectory dir, Expression<Func<TrainingTree, double>> mvaValue)
         {
             // Plot the weights. This can be used to plot signal vs background, ROC curves, etc.
-            source
+            var weights = source
                 .Select(j => TrainingUtils.TrainingTreeConverter.Invoke(j))
                 .Select(j => Tuple.Create(mvaValue.Invoke(j), j.Weight))
                 .FuturePlot(TrainingEventWeightFine.NameFormat, TrainingEventWeightFine.TitleFormat, TrainingEventWeightFine, "All")
@@ -121,9 +121,13 @@ namespace TrainingTestResults
             var plots = plotsFromJS
                 .Select(myp => myp.FromType<JetStream, Tuple<JetStream, double>>(jinfo => jinfo.Item1, weight: jinfo => jinfo.Item2 * jinfo.Item1.Weight));
 
-            // We want weighted and unweighted plots here.
+            // We want weighted and unweighted plots here. We first have to normalize the weighting to be from 0 to 1.
+            var firstNonZeroBinValue = weights.Value.FindNonZeroBinValue();
+            var lastNonZeroBinValue = weights.Value.FindNonZeroBinValue(HistogramUtils.BinSearchOrder.HighestBin);
+            var scaleing = 1.0 / (lastNonZeroBinValue - firstNonZeroBinValue);
+
             var mvaWeithedJetStream = source
-                .Select(j => Tuple.Create(j, j.Weight * mvaValue.Invoke(TrainingUtils.TrainingTreeConverter.Invoke(j))));
+                .Select(j => Tuple.Create(j, j.Weight * (mvaValue.Invoke(TrainingUtils.TrainingTreeConverter.Invoke(j)) - firstNonZeroBinValue)*scaleing));
             var weithedJetStream = source
                 .Select(j => Tuple.Create(j, j.Weight));
 
---
+++ libDataAccess/Utils/HistogramUtils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libDataAccess.Utils
+{
+    public static class HistogramUtils
+    {
+        /// <summary>
+        /// Which direction should we scan bins from?
+        /// </summary>
+        public enum BinSearchOrder
+        {
+            LowestBin,
+            HighestBin
+        }
+
+        /// <summary>
+        /// find the first bin that is nonzero.
+        /// </summary>
+        /// <param name="histo"></param>
+        /// <returns></returns>
+        public static double FindNonZeroBinValue(this ROOTNET.Interface.NTH1 histo, BinSearchOrder order = BinSearchOrder.LowestBin)
+        {
+            var ibin = order == BinSearchOrder.LowestBin
+                ? histo.FindFirstBinAbove()
+                : histo.FindLastBinAbove();
+            return histo.Xaxis.GetBinCenter(ibin);
+        }
+    }
+}
