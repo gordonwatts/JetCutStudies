51a689cffab1caaa0efe914e7f6fe8595d0070e2
Gordon Watts <gordon@gordonandpaula.net>
2016-07-03 04:48:19 -0400

    First go at geting at the results that come out of the generic training up on jenkins.

---
+++ JetMVATraining/summarize-optimization-runs.ps1
@@ -0,0 +1,34 @@
+#
+# Given a list of jobs, extract the run optimization numbers from the log files.
+# This will require accessing the internet and the installation of the Jenkins powershell
+# access package (see https://github.com/gordonwatts/JenkinsAccess).
+#
+
+# The optimization jobs to scan over.
+$firstJob = 418
+$lastJob = 451
+
+# Some helper functions
+function Clean-PropertyValue
+{
+	if ($args[0] -eq """""") { "" } else { $args[0] }
+}
+
+# Grab the results from each job, cache them locally
+
+$results = @()
+foreach ($jobId in $firstJob..$lastJob) {
+	$jobInfo = Find-JenkinsJob -JobUri http://jenks-higgs.phys.washington.edu:8080/view/LLP/job/CalR-JetMVATraining/ -JobId $jobId
+	if ($jobInfo.JobState -eq "Success") {
+		$jobResults = @{}
+		$jobInfo.Parameters.Keys | % {$jobResults[$_] = Clean-PropertyValue $jobInfo.Parameters[$_]}
+		$jobInfo `
+			| Get-JenkinsBuildLogfile `
+			| select -Last 80 `
+			| % { [regex]::match($_, "efficiency for BDT ([^:]+): (.+)\.$") } `
+			| ? { $_.Success} `
+			| % { @{$_.Groups[1].Value = Clean-PropertyValue $_.Groups[2].Value} } `
+			| % { $jobResults += $_}
+		Write-Output $jobResults
+	}
+}
