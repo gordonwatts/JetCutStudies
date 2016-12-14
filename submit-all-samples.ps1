7dd2eb0dc276b92d2de6257b28ed094f480184ae
Gordon Watts <gordon@gordonandpaula.net>
2016-12-14 09:36:10 -0800

    Revert "Starting to add the mc15c samples."
    This reverts commit 426fd730e822ac602d07c53fda97aaf47892f2c0.

--- submit-all-samples.ps1
+++ submit-all-samples.ps1
@@ -2,15 +2,15 @@
 # Submit all the samples to run
 #
 
-$jobName = "DiVertAnalysis"
-$jobVersion = 9
+$jobName = "DiVertAnalysisNP"
+$jobVersion = 2
 
 # Rucio command used to search for some of these:
 # -bash-4.1$ rucio list-dids mc15_13TeV:*AOD*e5102* | grep CONTAINER
 
 . .\script-utils.ps1
 
-$samples = findDS("mc15c+signal")
+$samples = findDS("extraptest")
 
 # Submit each one for processing
 foreach ($s in $samples) {
