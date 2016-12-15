628cb65f8f8ec526c47f6f7a290ce6ccd728dac7
Gordon Watts <gordon@gordonandpaula.net>
2016-12-15 14:06:08 -0800

    Turn into a real pipe-line script
    Part of the whole upgrade of processing items "correctly".

--- download-local-copy-of-all-ds.ps1
+++ download-local-copy-of-all-ds.ps1
@@ -2,29 +2,51 @@
 # Download a copy of all datasets
 #
 
-$jobName = "DiVertAnalysisNP"
-$jobVersion = 2
+[CmdletBinding()]
+Param(
+  [Parameter(Mandatory=$True, HelpMessage="The samples to submit against", Position=1, ValueFromPipeline=$True)]
+  [string]$samples,
+  [Parameter(HelpMessage="Get a single file, locally")]
+  [Switch]$DownloadFileLocally
+)
 
-# Get teh list of samples from the csv file
+begin {
+	# What we will be working with
+	$jobName = "DiVertAnalysis"
+	$jobVersion = 11
 
-. .\script-utils.ps1
+	# build extra download flags
+	$flags = ""
+	if ($DownloadFileLocally) {
+		$flags = "-nFiles 1 -Location Local"
+	}
+	
+	# Hold onto jobs that have been started
+	$downloadJobs = @()
+}
 
-$samples = findDS("extraptest")
-
-$job = $samples | % {
+process {
+	# Make sure we aren't trying to do too many things at once.
     while (@(Get-Job -State Running).Count -ge 4) {
+		Get-Job -State Running
         Start-Sleep -Seconds 2
     }
+	
+	# Kick off a download job if the job is finished.
 	$j = Get-GRIDJobInfo -JobStatus -JobName $jobName -JobVersion $jobVersion -DatasetName $_
 	if (($j -ne "finished") -and ($j -ne "done")) {
 		Write-Host "Skipping $_ - it is in state $j"
 	} else {
 		Write-Host "Starting $_"
-		Start-Job -Name $_ -ScriptBlock {
-			param ($sample, $jobName, $jobVersion)
-			Get-GRIDDataset -JobName $jobName -JobVersion $jobVersion -JobSourceDatasetName $sample
-			#-Location Local -nFiles 1 
-		} -ArgumentList $_, $jobName, $jobVersion
+		$job = Start-Job -Name $_ -ScriptBlock {
+			param ($sample, $jobName, $jobVersion, $flags)
+			Invoke-Expression "Get-GRIDDataset -JobName $jobName -JobVersion $jobVersion -JobSourceDatasetName $sample $flags"
+		} -ArgumentList $_, $jobName, $jobVersion, $flags
+		$downloadJobs += $job
 	}
 }
-$jobsdone = $job | Wait-Job | Receive-Job
+
+end {
+	# Wait for all jobs to finish.
+	$downloadJobs | Wait-Job | Receive-Job
+}
