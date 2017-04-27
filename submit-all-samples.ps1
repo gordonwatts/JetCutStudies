07497c603e4aa6fc4cec09ab24a24b6453d2bd45
Gordon Watts <gordon@gordonandpaula.net>
2017-04-27 14:53:55 -0700

    Make the submission more efficient by making the invoke pipe-line continuous

--- submit-all-samples.ps1
+++ submit-all-samples.ps1
@@ -10,13 +10,24 @@ Param(
 #
 # Now, process everything
 #
+begin {
+  $allSamples = @()
+}
+
+# Accumulate everything we are going to submit.
+# We are making the assumption that the generation of these sample names is much slower than the submission time.
+# Submission can take 5 minutes. And doing them at once can make the process more efficient.
 process {
+  $allSamples += $samples
+}
+
+end {
 
 	$jobName = "DiVertAnalysis"
 	$jobVersion = 14
 
 	# Submit each one for processing
-	$samples `
+	$allSamples `
 		| Invoke-GRIDJob -JobName $jobName -JobVersion $jobVersion `
 		| % {@{Dataset = $_.Name
 				Task = $_.ID
