f2fe04ee4024582037d2800fa6563be5cf4b929e
Gordon Watts <gordon@gordonandpaula.net>
2017-10-04 08:12:11 -0700

    Update to most recent LINQToTTree to make sure we are dumping output as we go.

--- TMVAUtilities/Training.cs
+++ TMVAUtilities/Training.cs
@@ -580,7 +580,7 @@ namespace TMVAUtilities
             script.AppendLine("f->EvaluateAllMethods();");
 
             // Now, run the script!
-            LocalBashHelpers.RunROOTInBash("training", script.ToString(), new DirectoryInfo(System.Environment.CurrentDirectory));
+            LocalBashHelpers.RunROOTInBash("training", script.ToString(), new DirectoryInfo(System.Environment.CurrentDirectory), s => Console.WriteLine(s));
 
             // Write out the hash value
             using (var wr = hashFile.CreateText())
