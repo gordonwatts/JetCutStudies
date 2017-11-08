ecab925c22fad4b0cffa3d2301a7415436367cf7
Gordon Watts <gordon@gordonandpaula.net>
2017-11-08 07:49:26 -0800

    Turn on verbose checking for training to see if we can figure out what we are messing up on the Jenkins guy

--- JetMVAClassifierTraining/Program.cs
+++ JetMVAClassifierTraining/Program.cs
@@ -157,7 +157,7 @@ namespace JetMVAClassifierTraining
                 methods.Add(m1);
 
                 // Do the training
-                var trainingResult = training.Train("JetMVAClassifier");
+                var trainingResult = training.Train("JetMVAClassifier", verbose: s => Console.WriteLine(s));
 
                 // Build a job name and coppy everything over to that for easy reference and so we can find the results.
                 var jobNameBuilder = new StringBuilder();
