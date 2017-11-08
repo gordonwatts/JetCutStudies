41e1e46f0df1ec2e55bcf92a31756bca751228a1
Gordon Watts <gordon@gordonandpaula.net>
2017-11-08 09:23:28 -0800

    Update how we get BIB data and alter the number of trees we train with.

--- JetMVAClassifierTraining/Program.cs
+++ JetMVAClassifierTraining/Program.cs
@@ -19,6 +19,7 @@ using static libDataAccess.PlotSpecifications;
 using static libDataAccess.Utils.CommandLineUtils;
 using static libDataAccess.Utils.SampleUtils;
 using static libDataAccess.Utils.FutureConsole;
+using static libDataAccess.Utils.BIBSamples;
 using LINQToTTreeLib.Files;
 
 namespace JetMVAClassifierTraining
@@ -62,6 +63,9 @@ namespace JetMVAClassifierTraining
 
             [Option("TrainEventsBIB15", HelpText = "How many events from data15 should be used in the training for bib15 (-1 is all, 0 is none)?", Default = -1)]
             public int EventsToUseForTrainingAndTestingBIB15 { get; set; }
+
+            [Option("nTrees", HelpText = "How many trees should be trained in the boosting?", Default = 800)]
+            public int MaxTreesForTraining { get; set; }
 
             [Option("PrecisionValue", HelpText ="The fraction of events in each sample to use when calculating the training precision", Default = 0.90)]
             public double PrecisionValue { get; set; }
@@ -145,6 +149,7 @@ namespace JetMVAClassifierTraining
                     .Option("MinNodeSize", options.BDTLeafMinFraction.ToString())
                     .Option("nCuts", "200")
                     .Option("BoostType", "Grad")
+                    .Option("NTrees", options.MaxTreesForTraining.ToString())
                     .Option("NegWeightTreatment", "IgnoreNegWeightsInTraining")
                     ;
 
@@ -157,7 +162,7 @@ namespace JetMVAClassifierTraining
                 methods.Add(m1);
 
                 // Do the training
-                var trainingResult = training.Train("JetMVAClassifier", verbose: s => Console.WriteLine(s));
+                var trainingResult = training.Train("JetMVAClassifier", verbose: s => Console.WriteLine(s), forceTraining: true);
 
                 // Build a job name and coppy everything over to that for easy reference and so we can find the results.
                 var jobNameBuilder = new StringBuilder();
@@ -291,91 +296,6 @@ namespace JetMVAClassifierTraining
 #endif
                 // Done. Dump all output.
                 Console.Out.DumpFutureLines();
-            }
-        }
-
-        /// <summary>
-        /// Grab the BIB samles
-        /// </summary>
-        /// <param name="requestedNumberOfEvents">-1 for everything, or a number of requested</param>
-        /// <param name="bib_tag">The tag name we should use to do the lookup</param>
-        /// <returns></returns>
-        private static IQueryable<JetStream> GetBIBSamples(int requestedNumberOfEvents, DataEpoc epoc, double pTCut, string[] avoidPlaces = null)
-        {
-            // If no events, then we need to just return everything
-            if (requestedNumberOfEvents == 0)
-            {
-                return null;
-            }
-
-            // Fetch all the data samples
-            var dataSamples = SampleMetaData.AllSamplesWithTag(epoc == DataEpoc.data15 ? "data15_p2950" : "data16_p2950");
-
-            // BiB files are funny - the data is far and few between. So we need to boost the number of files
-            // we look at.
-            var oldNFiles = Files.NFiles;
-            if (oldNFiles <= 1)
-            {
-                Files.NFiles = 2;
-            }
-            try
-            {
-
-                // If we have a limitation on the number of events, then we need to measure our the # of events.
-                int countOfEvents = 0;
-                int countOfEventsOneBack = 0;
-                dataSamples = dataSamples
-                    .TakeWhile(s =>
-                    {
-                        if (requestedNumberOfEvents < 0)
-                        {
-                            return true;
-                        }
-                        var q = Files.GetSampleAsMetaData(s, avoidPlaces: avoidPlaces);
-                        countOfEventsOneBack = countOfEvents;
-                        countOfEvents += q.AsBeamHaloStream(epoc)
-                                            .AsGoodJetStream(pTCut)
-                                            .Count();
-                        return countOfEvents < requestedNumberOfEvents;
-                    })
-                    .ToArray();
-
-                // The following is the tricky part. Now that we have a list of events, it is not likely that we have found a file boundary
-                // that matches the number of events. So we will have to do this a little carefully.
-
-                SampleMetaData theLastSample = null;
-                IEnumerable<SampleMetaData> allBut = dataSamples;
-                if (countOfEvents > 0 && countOfEvents > requestedNumberOfEvents)
-                {
-                    // Take up to the last one.
-                    allBut = dataSamples.Take(dataSamples.Count() - 1);
-                    theLastSample = dataSamples.Last();
-                }
-
-                var data1 = allBut
-                    .SamplesAsSingleQueriable(avoidPlaces)
-                    .AsBeamHaloStream(epoc)
-                    .AsGoodJetStream(pTCut);
-
-                var data = theLastSample == null ? data1
-                    : data1.Concat(Files.GetSampleAsMetaData(theLastSample, avoidPlaces: avoidPlaces).AsBeamHaloStream(epoc).AsGoodJetStream(pTCut).Take(requestedNumberOfEvents - countOfEventsOneBack));
-
-                // Check that we did ok. This will prevent errors down the line that are rather confusing.
-                if (countOfEvents < requestedNumberOfEvents)
-                {
-                    Console.WriteLine($"Warning - unable to get all the events requested for {epoc.ToString()}. {countOfEvents} were found, and {requestedNumberOfEvents} events were requested.");
-                }
-                if (countOfEvents == 0 && requestedNumberOfEvents > 0)
-                {
-                    throw new InvalidOperationException($"Unable to get any events for {epoc.ToString()}!");
-                }
-
-                return data;
-            }
-            finally
-            {
-                // Reset the number of files we are looking at.
-                Files.NFiles = oldNFiles;
             }
         }
 
