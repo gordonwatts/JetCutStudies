b4a0210033d42c5aa78e2adcf462c9bf2e2f6916
Gordon Watts <gordon@gordonandpaula.net>
2017-11-08 22:14:28 -0800

    Minor update to run over a few more things.

--- ScanSamples.ps1
+++ ScanSamples.ps1
@@ -4,10 +4,18 @@
 Param(
 )
 
-runit(200)
-
 function runit ([int] $nTrees)
 {
 	C:\Users\Gordon\Documents\Code\calratio2017\JetCutStudies\JetMVAClassifierTraining\bin\x86\Debug\JetMVAClassifierTraining.exe --BDTMaxDepth 50 --BDTLeafMinFraction 1 --TrainingEvents 1500000 "" --TrainEventsBIB16 0 --TrainEventsBIB15 -1 --SmallTestingMenu --nTrees $nTrees
 	cp JetMVAClassifierTraining.root JetMVAClassifierTraining-nT$nTrees.root
 }
+
+cd C:\Users\Gordon\Documents\Code\calratio2017\JetCutStudies\JetMVAClassifierTraining\bin
+#runit(100)
+#runit(200)
+#runit(300)
+#runit(400)
+#runit(500)
+#runit(600)
+#runit(700)
+runit(800)
